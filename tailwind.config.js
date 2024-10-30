6f86d6d2e7d58b0fcf0b2a31e32bf845d6dcd944
Itzel Quezada <atl024.itzel@gmail.com>
2024-10-30 05:36:54 -0600

    Home

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
+    './pages/**/*.{html,js}',
+    './components/**/*.{html,js}',
+    'public/agregar_evento.html',
     'public/index.html',
     'public/landing.html',
     'src/estilos.css',
-
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        cprimary: '#7e894e',
+        csecondary: '#000',
+        cthird: '#cfd1b4',
+        cfourth: '#cfd1b4',
+      }
+    },
   },
   plugins: [],
 }
